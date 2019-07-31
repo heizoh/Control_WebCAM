a9428f6caa44b13ed4a6acb7506aacfec6367cde
mstk <mstk0531@gmail.com>
2019-07-31 22:57:59 +0900

    20190731

--- Control_WebCAM/Form1.cs
+++ Control_WebCAM/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenCvSharp;
+using OpenCvSharp.Extensions;
 
 namespace Control_WebCAM
 {
@@ -17,6 +18,7 @@ namespace Control_WebCAM
         Bitmap bmp;
         Mat Fr;
         Graphics Gr;
+        Rectangle rect;
 
         int HEIGHT = 480;
         int WIDTH = 720;
@@ -37,7 +39,8 @@ namespace Control_WebCAM
             Fr = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
 
             bmp = new Bitmap(Fr.Cols, Fr.Rows, (int)Fr.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, Fr.Data);
-
+            pictureBox1.Image = bmp;
+            rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
             Gr = pictureBox1.CreateGraphics();
             
         }
@@ -48,34 +51,52 @@ namespace Control_WebCAM
 
         
 
-        private bool waitCancel()
+        private bool WaitCancel()
         {
-            CV.Grab();
 
             Invoke((MethodInvoker)delegate
-            {
-                NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(CV.CvPtr, Fr.CvPtr);
-                
-                Gr.DrawImage(bmp, 0, 0, Fr.Cols, Fr.Rows);
-                pictureBox1.Refresh();
-            });
+                {
+                    
+                    //if(pictureBox1.Image != null)
+                    //{
+                    //    pictureBox1.Image.Dispose();
+                    //}
+
+                    pictureBox1.Image = BitmapConverter.ToBitmap(Fr);
+
+                    //System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+                    //NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(CV.CvPtr, bmpData.Scan0);
+                    //bmp.UnlockBits(bmpData);
+                    //Gr.DrawImage(bmp, 0, 0, Fr.Cols, Fr.Rows);
+                    pictureBox1.Refresh();
+                });
             return true;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
             bool Frg = true;
-            while (Frg)
+            Task.Run(() =>
             {
-                Task.Run(() =>
+                while (Frg)
                 {
 
-                    Frg = waitCancel();
-                    System.Threading.Thread.Sleep(100);
+                    CV.Read(Fr);
 
-                });
+                    Frg = WaitCancel();
+                    System.Threading.Thread.Sleep(50);
+                }
+            });
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(CV!=null)
+            {
+                CV.Dispose();
             }
-
+            
         }
     }
 }
